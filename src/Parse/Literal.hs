8f6baf5af02ea6d45118774245686cdaa5854bc8
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-02-01 11:25:16 +0000

    number parsing

--- src/Parse/Literal.hs
+++ src/Parse/Literal.hs
@@ -1,16 +1,17 @@
 module Parse.Literal where
-import Text.ParserCombinators.Parsec ((<|>))
-import qualified Text.Parsec as P
-import qualified Text.ParserCombinators.Parsec as P
+
 import AST.Source
+import Debug.Trace
+import qualified Text.Parsec as P
+import Text.ParserCombinators.Parsec ((<|>))
+import qualified Text.ParserCombinators.Parsec as P
 
 string :: P.Parser Expr
 string = do
-    _ <- P.char '"'
-    body <- P.many (P.noneOf "\"")
-    _ <- P.char '"'
-    return $ StringE body
-
+  _ <- P.char '"'
+  body <- P.many (P.noneOf "\"")
+  _ <- P.char '"'
+  return $ StringE body
 
 char :: P.Parser Expr
 char = do
@@ -19,6 +20,14 @@ char = do
   _ <- P.char '\''
   return $ CharE content
 
-literal :: P.Parser Expr
-literal = string <|> char 
+number :: P.Parser Expr
+number = do
+  integer <- P.many1 P.digit
+  float <- P.optionMaybe $ P.char '.' >> P.many1 P.digit
+  let intPart = read integer :: Integer
+  return $ case trace "float" float of
+    Nothing -> IntE intPart
+    Just floatPart -> FloatE $ fromIntegral intPart + read ("0." ++ floatPart)
 
+literal :: P.Parser Expr
+literal = string <|> char <|> number
