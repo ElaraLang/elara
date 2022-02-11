46debc02e915b0fd4a27f130ea27620b10466f31
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-02-11 14:08:56 +0000

    monad

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,22 +7,24 @@ import Control.Monad (forM_)
 import Parse.Lexer
 import Parse.Parser
 import Parse.Utils
+import Data.List (intercalate)
 
 someFunc :: IO ()
 someFunc = do
   content <- readFile "source.elr"
   let tokens = evalP readTokens content
   print tokens
-  let lines = parse content
-  forM_ lines $ \(ExpressionL ast) -> do
+  let codeLines = parse content
+  forM_ codeLines $ \(ExpressionL ast) -> do
     putStrLn $ showASTNode ast
 
 showASTNode :: Expression -> String
-showASTNode (FuncApplicationE a b) = "(" ++ showASTNode a ++ " " ++ showASTNode b ++ ")"
+showASTNode (FuncApplicationE a b) = showASTNode a ++ " " ++ showASTNode b
 showASTNode (LetE pattern value) = "let " ++ showPattern pattern ++ " = " ++ showASTNode value
 showASTNode (IdentifierE i) = showIdentifier i
-showASTNode (ConstE const) = showConst const
-showASTNode (BlockE expressions) = "{" ++ (unwords $ map showASTNode expressions) ++ "}"
+showASTNode (ConstE val) = showConst val
+showASTNode (BlockE expressions) = "{" ++ (intercalate "; " $ map showASTNode expressions) ++ "}"
+showASTNode (InfixApplicationE op a b) = showASTNode a ++ " " ++ showIdentifier op ++ " " ++ showASTNode b
 showASTNode a = show a
 
 showConst :: Constant -> String
