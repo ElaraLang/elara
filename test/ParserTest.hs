cd12035130b992b36e6724c552bf5a875078fed5
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-02-15 17:29:03 +0000

    add a unit test for cons precedence

--- test/ParserTest.hs
+++ test/ParserTest.hs
@@ -65,6 +65,9 @@ spec = describe "Test Parser" $ do
 
   it "Parses a function definition with a cons pattern argument" $ do
     "let f (x:xs) = x" <=> LetE (FunctionP (NormalIdentifier "f") [ConsP (IdentifierP $ NormalIdentifier "x") (IdentifierP $ NormalIdentifier "xs")]) (IdentifierE (NormalIdentifier "x"))
+    
+  it "Parses cons operator precedence correctly" $ do
+    "f x : map f xs" <=> ConsE (FuncApplicationE (IdentifierE (NormalIdentifier "f")) (IdentifierE (NormalIdentifier "x"))) (FuncApplicationE (FuncApplicationE (IdentifierE $ NormalIdentifier "map") (IdentifierE $ NormalIdentifier "f")) (IdentifierE (NormalIdentifier "xs")))
 
 infix 4 <=>
 
