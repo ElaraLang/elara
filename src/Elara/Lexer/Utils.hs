c05224b6166484c3c2748e2c1b7aee660412fe05
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-03-28 22:44:30 +0100

    new error just dropped

--- src/Elara/Lexer/Utils.hs
+++ src/Elara/Lexer/Utils.hs
@@ -4,7 +4,7 @@
 module Elara.Lexer.Utils where
 
 import Codec.Binary.UTF8.String (encodeChar)
-import Control.Lens (makeLenses, use, view, (^.))
+import Control.Lens (makeLenses, over, to, use, view, (^.))
 import Data.List.NonEmpty (span, (<|))
 import Data.Text qualified as T
 import Data.Word (Word8)
@@ -30,49 +30,68 @@ data AlexInput = AlexInput
     }
     deriving (Show)
 
+data IndentInfo = IndentInfo
+    { _indent :: Int
+    , _indentPos :: RealPosition
+    }
+
 data ParseState = ParseState
     { _input :: AlexInput
     , _lexSC :: Int -- lexer start code
     , _stringBuf :: String -- temporary storage for strings
     , _pendingTokens :: [Lexeme] -- right now used when Parser consumes the lookeahead and decided to put it back
-    , _indentStack :: NonEmpty Int -- stack of indentation levels
+    , _indentStack :: NonEmpty IndentInfo -- stack of indentation levels
     , _pendingPosition :: TokPosition -- needed when parsing strings, chars, multi-line strings
     }
-    deriving (Show)
 
 makeLenses ''AlexInput
+makeLenses ''IndentInfo
 makeLenses ''ParseState
 
 type LexMonad :: Type -> Type
 type LexMonad a = Sem [State ParseState, Error LexerError] a
 
+mkIndentInfo :: Int -> LexMonad IndentInfo
+mkIndentInfo i = do
+    pos <- use (input . position)
+    pure (IndentInfo i pos)
+
 data LexerError
     = -- | When an element is indented more than expected
       TooMuchIndentation
-        Int
+        IndentInfo
         -- ^ The expected indentation
-        (Maybe Int)
+        (Maybe IndentInfo)
         -- ^ The potential further indentation
         Int
         -- ^ The actual indentation
         ParseState
         -- ^ The current state of the lexer
-    deriving (Show)
 
 instance ReportableError LexerError where
     report (TooMuchIndentation expected further actual s) = do
         let fp = view (input . filePath) s
         let pos = view (input . position) s
-        let msg = "Unexpected indentation. Expected " <> show expected <> " spaces, but got " <> show actual <> " spaces."
+        let msg = "Unexpected change in indentation. Expected " <> show (expected ^. indent) <> " spaces, but got " <> show actual <> " spaces."
         let hint = case further of
                 Nothing -> "Try removing the extra indentation."
-                Just f -> "Try removing the extra indentation or indenting the line by " <> show f <> " spaces."
+                Just f -> "Try removing the extra indentation or indenting the line by " <> show (f ^. indent - actual) <> " space(s)."
+
+        let baseHints =
+                [ (positionToDiagnosePosition fp pos, This "this line is indented incorrectly")
+                , (positionToDiagnosePosition fp (expected ^. indentPos), Maybe "If the decrease in indentation is intentional, the problematic line should line up with this line.")
+                ]
+        let furtherHints = case further of
+                Nothing -> baseHints
+                Just f -> (positionToDiagnosePosition fp (f ^. indentPos), Maybe ("an offside rule begins here (column " <> show (f ^. indent) <> "). If you think the problematic line is \"related\" to this line, make sure they line up.")) : baseHints
         writeReport $
             Err
                 (Just Codes.tooMuchIndentation)
                 msg
-                [(positionToDiagnosePosition fp pos, This "this line is indented too far")]
-                [Note "When using lightweight syntax, the level of indentation is very important. Currently, I can't tell what expression this line is supposed to be a part of. ", Hint hint]
+                furtherHints
+                [ Note "When using lightweight syntax, the level of indentation is very important. Currently, I can't tell what expression this line is supposed to be a part of as it doesn't line up with anything, and didn't appear in a place where indentation can begin."
+                , Hint hint
+                ]
 
 initialState :: FilePath -> String -> ParseState
 initialState fp s =
@@ -88,7 +107,7 @@ initialState fp s =
         , _lexSC = 0
         , _stringBuf = ""
         , _pendingTokens = []
-        , _indentStack = 0 :| []
+        , _indentStack = IndentInfo 0 (Position 1 1) :| []
         , _pendingPosition = Position 1 1
         }
 
@@ -105,18 +124,19 @@ startWhite _ str = do
     let indentation = T.length $ T.dropWhile (== '\n') str
     s <- get
     let indents@(cur :| _) = s ^. indentStack
-    case indentation `compare` cur of
+    case indentation `compare` (cur ^. indent) of
         GT -> do
             fakeLb <- fake TokenLeftBrace
-            put s{_indentStack = indentation <| indents, _pendingTokens = fakeLb : _pendingTokens s}
+            indentInfo <- mkIndentInfo indentation
+            put s{_indentStack = indentInfo <| indents, _pendingTokens = fakeLb : _pendingTokens s}
             pure Nothing
         LT -> do
             -- If the indentation is less than the current indentation, we need to close the current block
-            case span (> indentation) indents of
+            case span (view (indent . to (> indentation))) indents of
                 (pre, top : xs) -> do
                     -- pre is all the levels that need to be closed, top is the level that we need to match
                     fakeClosings <- sequenceA [fake TokenRightBrace, fake TokenSemicolon]
-                    if top == indentation
+                    if top ^. indent == indentation
                         then
                             put
                                 s
@@ -133,7 +153,7 @@ cleanIndentation :: LexMonad [Lexeme]
 cleanIndentation = do
     indentStack <- use indentStack
     fakeClosings <- sequenceA [fake TokenRightBrace]
-    modify $ \s -> s{_indentStack = 0 :| []}
+    modify $ \s -> s{_indentStack = IndentInfo 0 (Position 1 1) :| []}
     pure $ init indentStack >>= const fakeClosings
 
 -- The functions that must be provided to Alex's basic interface
