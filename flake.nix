3072a334fd3c97dde86fad1b8d1934dc897c670d
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-07-11 00:29:25 +0100

    restart if source.elr changes

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
           run = {
             description = "Run the project with ghcid auto-recompile";
             exec = ''
-              ghcid -c "stack repl --ghc-options=\"-ferror-spans -fdiagnostics-color=always\"" --warnings -T :main --colour=always --restart='source.elr,prelude.elr'
+              ghcid -c "stack repl --ghc-options=\"-ferror-spans -fdiagnostics-color=always\"" --warnings -T :main --colour=always --restart=source.elr
             '';
             category = "Primary";
           };
