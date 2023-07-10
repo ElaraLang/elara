5e5ed5ec060ce2c42e59a5ac82540fb10e83f2e2
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-07-11 00:27:03 +0100

    we can reference variables now :)

--- src/Elara/Emit.hs
+++ src/Elara/Emit.hs
@@ -30,56 +30,56 @@ type Emit r = Members '[Reader JVMVersion] r
 
 emitGraph :: forall r. Emit r => TopologicalGraph (Module Typed) -> Sem r [(ModuleName, ClassFile)]
 emitGraph g = do
-  let tellMod = emitModule >=> tell . one :: Module Typed -> Sem (Writer [(ModuleName, ClassFile)] : r) () -- this breaks without the type signature lol
-  fst <$> runWriter (traverseGraphRevTopologically_ tellMod g)
+    let tellMod = emitModule >=> tell . one :: Module Typed -> Sem (Writer [(ModuleName, ClassFile)] : r) () -- this breaks without the type signature lol
+    fst <$> runWriter (traverseGraphRevTopologically_ tellMod g)
 
 emitModule :: Emit r => Module Typed -> Sem r (ModuleName, ClassFile)
 emitModule m = do
-  let name = createModuleName (m ^. unlocatedModuleName)
-  version <- ask
+    let name = createModuleName (m ^. unlocatedModuleName)
+    version <- ask
 
-  let (fields, methods) = partitionEithers $ createMethodOrField <$> m ^. declarations
-  let methods' = methods ++ [generateMainMethod m | isMainModule m]
-  pure
-    ( m ^. unlocatedModuleName,
-      ClassFile
-        name
-        version
-        []
-        Nothing
-        []
-        fields
-        (createFieldInitialisers (m ^. declarations) : methods')
-        []
-    )
+    let (fields, methods) = partitionEithers $ createMethodOrField <$> m ^. declarations
+    let methods' = methods ++ [generateMainMethod m | isMainModule m]
+    pure
+        ( m ^. unlocatedModuleName
+        , ClassFile
+            name
+            version
+            []
+            Nothing
+            []
+            fields
+            (createFieldInitialisers (m ^. declarations) : methods')
+            []
+        )
   where
     createMethodOrField :: Declaration -> Either ClassFileField ClassFileMethod
     createMethodOrField d =
-      case d ^. unlocatedDeclarationBody' of
-        TypeDeclaration {} -> Left undefined
-        Value v ->
-          let (expr, type') = v ^. _Expr
-           in if typeIsValue type'
-                then Left $ ClassFileField [FPublic, FStatic] (d ^. unlocatedDeclarationName . to nameText) (generateFieldType type') []
-                else
-                  let descriptor@(MethodDescriptor _ returnType) = generateMethodDescriptor type'
-                   in Right $
-                        ClassFileMethod
-                          [MPublic, MStatic]
-                          (d ^. unlocatedDeclarationName . to nameText)
-                          descriptor
-                          [generateCodeAttribute v (if returnType == VoidReturn then (<> [Return]) else (<> [AReturn]))]
+        case d ^. unlocatedDeclarationBody' of
+            TypeDeclaration{} -> Left undefined
+            Value v ->
+                let (expr, type') = v ^. _Expr
+                 in if typeIsValue type'
+                        then Left $ ClassFileField [FPublic, FStatic] (d ^. unlocatedDeclarationName . to nameText) (generateFieldType type') []
+                        else
+                            let descriptor@(MethodDescriptor _ returnType) = generateMethodDescriptor type'
+                             in Right $
+                                    ClassFileMethod
+                                        [MPublic, MStatic]
+                                        (d ^. unlocatedDeclarationName . to nameText)
+                                        descriptor
+                                        [generateCodeAttribute v (if returnType == VoidReturn then (<> [Return]) else (<> [AReturn]))]
 
     generateCodeAttribute :: Expr -> ([Instruction] -> [Instruction]) -> MethodAttribute
     generateCodeAttribute e codeMod =
-      Code $
-        CodeAttributeData
-          { maxStack = 2, -- TODO: calculate this
-            maxLocals = 2, -- TODO: calculate this too
-            code = codeMod (generateCode e),
-            exceptionTable = [],
-            codeAttributes = []
-          }
+        Code $
+            CodeAttributeData
+                { maxStack = 2 -- TODO: calculate this
+                , maxLocals = 2 -- TODO: calculate this too
+                , code = codeMod (generateCode e)
+                , exceptionTable = []
+                , codeAttributes = []
+                }
 
 isMainModule :: Module Typed -> Bool
 isMainModule m = m ^. unlocatedModuleName == ModuleName ("Main" :| [])
@@ -87,82 +87,83 @@ isMainModule m = m ^. unlocatedModuleName == ModuleName ("Main" :| [])
 -- | Generates a main method, which merely loads a IO action field called main and runs it
 generateMainMethod :: Module Typed -> ClassFileMethod
 generateMainMethod m =
-  ClassFileMethod
-    [MPublic, MStatic]
-    "main"
-    ( MethodDescriptor
-        [ArrayFieldType (ObjectFieldType "java.lang.String")]
-        VoidReturn
-    )
-    [ Code $
-        CodeAttributeData
-          { maxStack = 2, -- TODO: calculate this
-            maxLocals = 2, -- TODO: calculate this too
-            code =
-              [ GetStatic (ClassInfoType (createModuleName (m ^. unlocatedModuleName))) "main" (ObjectFieldType "elara.IO"),
-                InvokeVirtual (ClassInfoType "elara.IO") "run" (MethodDescriptor [] VoidReturn),
-                Return
-              ],
-            exceptionTable = [],
-            codeAttributes = []
-          }
-    ]
+    ClassFileMethod
+        [MPublic, MStatic]
+        "main"
+        ( MethodDescriptor
+            [ArrayFieldType (ObjectFieldType "java.lang.String")]
+            VoidReturn
+        )
+        [ Code $
+            CodeAttributeData
+                { maxStack = 2 -- TODO: calculate this
+                , maxLocals = 2 -- TODO: calculate this too
+                , code =
+                    [ GetStatic (ClassInfoType (createModuleName (m ^. unlocatedModuleName))) "main" (ObjectFieldType "elara.IO")
+                    , InvokeVirtual (ClassInfoType "elara.IO") "run" (MethodDescriptor [] VoidReturn)
+                    , Return
+                    ]
+                , exceptionTable = []
+                , codeAttributes = []
+                }
+        ]
 
 createFieldInitialisers :: [Declaration] -> ClassFileMethod
 createFieldInitialisers decls =
-  ClassFileMethod
-    [MStatic]
-    "<clinit>"
-    (MethodDescriptor [] VoidReturn)
-    [ Code $ CodeAttributeData 255 255 (concatMap generateFieldInitialiser decls ++ [Return]) [] []
-    ]
+    ClassFileMethod
+        [MStatic]
+        "<clinit>"
+        (MethodDescriptor [] VoidReturn)
+        [ Code $ CodeAttributeData 255 255 (concatMap generateFieldInitialiser decls ++ [Return]) [] []
+        ]
   where
     generateFieldInitialiser :: Declaration -> [Instruction]
     generateFieldInitialiser d =
-      case d ^. unlocatedDeclarationBody' of
-        Value v ->
-          if typeIsValue (v ^. _Expr . _2)
-            then
-              let fieldClassName = createModuleName (d ^. unlocatedModuleName)
-                  fieldName = d ^. unlocatedDeclarationName . to nameText
-                  fieldDescriptor = generateFieldType (v ^. _Expr . _2)
-               in (generateCode v <> [PutStatic (ClassInfoType fieldClassName) fieldName fieldDescriptor])
-            else []
-        _ -> []
+        case d ^. unlocatedDeclarationBody' of
+            Value v ->
+                if typeIsValue (v ^. _Expr . _2)
+                    then
+                        let fieldClassName = createModuleName (d ^. unlocatedModuleName)
+                            fieldName = d ^. unlocatedDeclarationName . to nameText
+                            fieldDescriptor = generateFieldType (v ^. _Expr . _2)
+                         in (generateCode v <> [PutStatic (ClassInfoType fieldClassName) fieldName fieldDescriptor])
+                    else []
+            _ -> []
 
 createModuleName :: ModuleName -> QualifiedClassName
 createModuleName (ModuleName name) = QualifiedClassName (PackageName $ init name) (ClassName $ last name)
 
 invokeStaticVars :: Expr -> (ClassInfoType, Text, MethodDescriptor)
 invokeStaticVars (Expr (Located _ e, exprType)) =
-  case e of
-    AST.Var (Located _ (Global (Located _ (Qualified vn mn)))) ->
-      if typeIsValue exprType
-        then error "dunno about global vars"
-        else (ClassInfoType $ createModuleName mn, nameText vn, generateMethodDescriptor exprType)
-    other -> error (show other)
+    case e of
+        AST.Var (Located _ (Global (Located _ (Qualified vn mn)))) ->
+            if typeIsValue exprType
+                then error "dunno about global vars"
+                else (ClassInfoType $ createModuleName mn, nameText vn, generateMethodDescriptor exprType)
+        other -> error (show other)
 
 generateCode :: Expr -> [Instruction]
 generateCode (Expr (Located _ e, exprType)) =
-  case e of
-    FunctionCall
-      (Expr (Located _ (Var (Located _ v)), _))
-      (Expr (Located _ (AST.String "println"), _))
-        | v == fetchPrimitive ->
-            [ ALoad0,
-              InvokeStatic (ClassInfoType "elara.IO") "println" (MethodDescriptor [ObjectFieldType "java.lang.String"] (TypeReturn (ObjectFieldType "elara.IO")))
-            ]
-    AST.Var (Located _ (Global qn)) -> error "standalone var"
-    AST.String c -> [LDC (LDCString c)]
-    AST.Int i -> [LDC (LDCInt (fromIntegral i))]
-    FunctionCall f x -> do
-      let (a, b, c) = invokeStaticVars f
-      let outs = generateCode x
-      outs ++ [InvokeStatic a b c]
-    e -> error (show e)
+    case e of
+        FunctionCall
+            (Expr (Located _ (Var (Located _ v)), _))
+            (Expr (Located _ (AST.String "println"), _))
+                | v == fetchPrimitive ->
+                    [ ALoad0
+                    , InvokeStatic (ClassInfoType "elara.IO") "println" (MethodDescriptor [ObjectFieldType "java.lang.String"] (TypeReturn (ObjectFieldType "elara.IO")))
+                    ]
+        AST.Var (Located _ (Global (Located _ (Qualified n mn)))) -> [GetStatic (ClassInfoType $ createModuleName mn) (nameText n) (generateFieldType exprType)]
+        AST.String c -> [LDC (LDCString c)]
+        AST.Int i -> [LDC (LDCInt (fromIntegral i))]
+        FunctionCall f x -> do
+            let (a, b, c) = invokeStaticVars f
+            let outs = generateCode x
+            outs ++ [InvokeStatic a b c]
+        e -> error (show e)
 
--- | Determines if a type is a value type.
--- That is, a type that can be compiled to a field rather than a method.
+{- | Determines if a type is a value type.
+ That is, a type that can be compiled to a field rather than a method.
+-}
 typeIsValue :: Type a -> Bool
 typeIsValue (Scalar _ _) = True
 typeIsValue (Record _ _) = True
