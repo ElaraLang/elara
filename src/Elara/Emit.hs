f0f677f9f20e323d2b17a01997bebc62dbf3af2d
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-08-15 15:06:38 +0100

    now we need to do more core stuff

--- src/Elara/Emit.hs
+++ src/Elara/Emit.hs
@@ -167,7 +167,7 @@ generateCode ((Var (Normal (Id{idVarName = Global (Identity (Qualified vn mn)), 
     -- load static var
     let invokeStaticVars = (ClassInfoType $ createModuleName mn, vn, generateFieldType idVarType)
     pure [uncurry3 GetStatic invokeStaticVars]
-generateCode e@(App (Var (Normal (Id{idVarName = Global (Identity (Qualified vn mn)), idVarType = idVarType}))) arg) (Just expectedType) = do
+generateCode (App (Var (Normal (Id{idVarName = Global (Identity (Qualified vn mn)), idVarType = idVarType}))) arg) (Just expectedType) = do
     -- static function application
     let invokeStaticVars = (ClassInfoType $ createModuleName mn, vn, generateMethodDescriptor idVarType)
 
