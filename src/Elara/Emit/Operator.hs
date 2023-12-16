66e54c9e11284d9453d288b378384b1102762da5
Alexander Wood <alexander.wood@bristol.ac.uk>
2023-12-16 00:02:32 +0000

    yeah turns out we dont need to translate operator names

--- src/Elara/Emit/Operator.hs
+++ src/Elara/Emit/Operator.hs
@@ -6,27 +6,7 @@ type OpTranslation = (Text, Text)
 
 translations :: [OpTranslation]
 translations =
-    [ ("+", "plus")
-    , ("-", "minus")
-    , ("*", "times")
-    , ("/", "divide")
-    , ("%", "mod")
-    , ("==", "eq")
-    , ("!=", "neq")
-    , ("|>", "pipe")
-    , ("<", "lt")
-    , (">", "gt")
-    , ("<=", "lte")
-    , (">=", "gte")
-    , ("&", "bitand")
-    , ("&&", "and")
-    , ("|", "bitor")
-    , ("^", "bitxor")
-    , ("||", "or")
-    , ("!", "not")
-    , (">>", "then")
-    , (">>=", "bind")
-    ]
+    []
 
 translateOperatorName :: Text -> Text
 translateOperatorName name = foldr (uncurry T.replace) name translations
