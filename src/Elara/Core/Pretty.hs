f7fd7d50d3e94bf6748a12edfc09e5d64f0c4f1c
Alexander Wood <alexander.wood@bristol.ac.uk>
2023-12-16 00:22:17 +0000

    use proper forall in core

--- src/Elara/Core/Pretty.hs
+++ src/Elara/Core/Pretty.hs
@@ -111,7 +111,7 @@ instance Pretty Type where
 prettyTy :: Type -> Doc AnsiStyle
 prettyTy (FuncTy t1 t2) = prettyTy1 t1 <+> "->" <+> prettyTy t2
 prettyTy (AppTy l t2) | l == listCon = brackets (prettyTy2 t2)
-prettyTy (ForAllTy tv t) = "forall" <+> prettyTypeVariable True tv <> "." <+> prettyTy t
+prettyTy (ForAllTy tv t) = "∀" <> prettyTypeVariable False tv <> "." <+> prettyTy t
 prettyTy other = prettyTy1 other
 
 prettyTy1 :: Type -> Doc AnsiStyle
