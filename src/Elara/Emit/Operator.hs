bca752e15b5ce168895b41d41c1430a282551ef5
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-12-18 12:40:51 +0000

    i was incorrect about the operator names

--- src/Elara/Emit/Operator.hs
+++ src/Elara/Emit/Operator.hs
@@ -6,7 +6,29 @@ type OpTranslation = (Text, Text)
 
 translations :: [OpTranslation]
 translations =
-    []
+    map
+        (\(a, b) -> (a, b <> "_"))
+        [ ("+", "plus")
+        , ("-", "minus")
+        , ("*", "times")
+        , ("/", "divide")
+        , ("%", "mod")
+        , ("==", "eq")
+        , ("!=", "neq")
+        , ("|>", "pipe")
+        , ("<", "lt")
+        , (">", "gt")
+        , ("<=", "lte")
+        , (">=", "gte")
+        , ("&", "bitand")
+        , ("&&", "and")
+        , ("|", "bitor")
+        , ("^", "bitxor")
+        , ("||", "or")
+        , ("!", "not")
+        , (">>", "then")
+        , (">>=", "bind")
+        ]
 
 translateOperatorName :: Text -> Text
 translateOperatorName name = foldr (uncurry T.replace) name translations
