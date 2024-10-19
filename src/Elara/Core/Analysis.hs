34ac8e633e92cb8343b92f6389f7680c870d4eb0
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-10-18 20:37:03 -0400

    feat(core): :sparkles: Add a function to calculate ("infer"?) the type of a core expression

--- src/Elara/Core/Analysis.hs
+++ src/Elara/Core/Analysis.hs
@@ -4,6 +4,7 @@ import Elara.Core (CoreExpr, Expr (..), TyCon, Var (..), typeArity)
 import Elara.Core qualified as Core
 
 import Data.List (maximum)
+import Elara.Prim.Core (charCon, doubleCon, intCon, stringCon, unitCon)
 
 estimateArity :: CoreExpr -> Int
 estimateArity (Var (TyVar _)) = error "Type variable in expression"
@@ -25,3 +26,30 @@ findTyCon (Core.ConTy tc) = Just tc
 findTyCon (Core.ForAllTy _ t) = findTyCon t
 findTyCon (Core.AppTy t _) = findTyCon t
 findTyCon _ = Nothing
+
+exprType :: HasCallStack => CoreExpr -> Core.Type
+exprType (Var v) = varType v
+exprType (Lit l) = literalType l
+exprType (App f _) = case exprType f of
+    Core.FuncTy _ t -> t
+    t -> error $ "exprType: expected function type, got " <> show t
+exprType (TyApp f t) = case exprType f of
+    Core.ForAllTy tv t' -> Core.substTypeVar tv t t'
+    t' -> error $ "exprType: expected forall type, got " <> show t'
+exprType (Lam b e) = Core.FuncTy (varType b) (exprType e)
+exprType (TyLam _ e) = exprType e
+exprType (Let _ e) = exprType e
+exprType (Match _ _ alts) = case alts of
+    [] -> error "exprType: empty match"
+    (_, _, e) : _ -> exprType e
+
+varType :: Var -> Core.Type
+varType (TyVar tv) = Core.TyVarTy tv
+varType (Id _ t _) = t
+
+literalType :: Core.Literal -> Core.Type
+literalType (Core.Int _) = Core.ConTy intCon
+literalType (Core.String _) = Core.ConTy stringCon
+literalType (Core.Char _) = Core.ConTy charCon
+literalType (Core.Double _) = Core.ConTy doubleCon
+literalType Core.Unit = Core.ConTy unitCon
