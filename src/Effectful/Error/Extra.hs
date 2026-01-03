70dc00c46d69e5a7cce97ca675670b86adee1157
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-01-03 18:30:23 +0000

    refactor: Add HasCallStack to `fromEither`

--- src/Effectful/Error/Extra.hs
+++ src/Effectful/Error/Extra.hs
@@ -3,5 +3,5 @@ module Effectful.Error.Extra where
 import Effectful
 import Effectful.Error.Static qualified as Eff
 
-fromEither :: (Eff.Error e :> r, Show e) => Either e a -> Eff r a
+fromEither :: (HasCallStack, Eff.Error e :> r, Show e) => Either e a -> Eff r a
 fromEither = either Eff.throwError pure
