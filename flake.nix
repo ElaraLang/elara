0953b70c4983f8c9335da20b630d563712e8f78e
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-03-24 08:22:58 +0000

    fix(nix): Try fix ci

--- flake.nix
+++ flake.nix
@@ -63,222 +63,233 @@
             };
             pre-commit.settings.hooks.treefmt.enable = true;
           };
-        flake = hix.lib.flake (
-          { config, ... }:
-          {
-            compiler = "ghc912";
-            systems = import inputs.systems;
-            envs.dev.ghcid.enable = false;
-            outputs.devShells = {
-              # extending the default devshell to add the pre-commit hooks and some other nice things
-              default = config.pkgs.mkShell {
-                inputsFrom = [
-                  config.outputs.devShells.dev # the devshell that hix provides
-                  self.allSystems.${config.system}.pre-commit.devShell # the pre-commit devshell
-                ];
-                buildInputs = with config.pkgs; [
-                  just
-                  convco
-                  git-cliff
-                  nixfmt
-
-                  mdbook
-                  mdbook-d2
-                  d2
-                  nixd
-                ];
-              };
-            };
-
-            overrides =
+        flake =
+          let
+            hixFlake = hix.lib.flake (
+              { config, ... }:
               {
-                source,
-                hackage,
-                enable,
-                notest,
-                unbreak,
-                jailbreak,
-                ...
-              }:
-              {
-                h2jvm = notest (source.root h2jvm);
-                unix = enable "os-string";
-                directory = enable "os-string";
-                diagnose = enable "megaparsec-compat" (source.root inputs.diagnose);
-                incipit-base = jailbreak;
-                incipit-core = jailbreak;
-                ghc-tcplugins-extra = hackage "0.5" "sha256-mOzdicJevaXZdZS4/RA1hU3CWJXMFwMUfmEH3YxX4Q8=";
+                compiler = "ghc912";
+                systems = import inputs.systems;
+                envs.dev.ghcid.enable = false;
+                outputs.devShells = {
+                  # extending the default devshell to add the pre-commit hooks and some other nice things
+                  default = config.pkgs.mkShell {
+                    inputsFrom = [
+                      config.outputs.devShells.dev # the devshell that hix provides
+                      self.allSystems.${config.system}.pre-commit.devShell # the pre-commit devshell
+                    ];
+                    buildInputs = with config.pkgs; [
+                      just
+                      convco
+                      git-cliff
+                      nixfmt
 
-                optics-core = notest; # test fails on ghc 9.12
-                optics = notest; # test fails on ghc 9.12
-                generic-optics = notest; # test fails on ghc 9.12
-
-                dependent-hashmap = source.root inputs.dependent-hashmap;
-
-                effectful = jailbreak (hackage "2.6.1.0" "sha256-krNjGxqdbmFpt1g3anTd5ajGtYnyvGaG+AiDLfJN8No=");
-                effectful-core = jailbreak;
-                effectful-plugin = jailbreak;
-                co-log-effectful = jailbreak (unbreak);
-              };
-            packages = {
-              elara = {
-                buildInputs = pkgs: [ pkgs.alex ];
-                src = ./.;
-                description = "See README for more info";
-                cabal = {
-                  author = "Alexander Wood";
-                  build-type = "Simple";
-                  copyright = "2022 Alexander Wood";
-                  license = "MIT";
-                  license-file = "LICENSE";
-                  version = "0.1.0";
-                  meta = {
-                    maintainer = "Alexander Wood <alexljwood24@hotmail.co.uk>";
-                    homepage = "https://github.com/ElaraLang/elara#readme";
-                    synopsis = "See README for more info";
-                  };
-                  language = "GHC2024";
-                  prelude = {
-                    enable = true;
-                    package = "relude";
-                    module = "Prelude";
-                  };
-                  default-extensions = [
-                    "OverloadedStrings"
-                    "OverloadedRecordDot"
-                    "TypeFamilies"
-                    "LambdaCase"
-                    "ImportQualifiedPost"
-                    "DeriveDataTypeable"
-                    "DataKinds"
-                    "DeriveFunctor"
-                    "TypeApplications"
-                    "PartialTypeSignatures"
-                  ];
-                  dependencies = [
-                    "aeson"
-                    "algebraic-graphs"
-                    "array"
-                    "binary"
-                    "bytestring"
-                    "co-log-core"
-                    "co-log-effectful"
-                    "containers"
-                    "dependent-sum-template"
-                    "diagnose"
-                    "directory"
-                    "dependent-hashmap"
-                    "effectful >= 2.6.1.0"
-                    "effectful-core"
-                    "effectful-plugin"
-                    "effectful-th"
-                    "filepath"
-                    "generic-optics"
-                    "h2jvm"
-                    "hashable"
-                    "kind-generics-th"
-                    "lens"
-                    "lifted-base"
-                    "matrix"
-                    "megaparsec"
-                    "mtl"
-                    "optics"
-                    "parser-combinators"
-                    "pretty-simple"
-                    "prettyprinter"
-                    "prettyprinter-ansi-terminal"
-                    "process"
-                    "relude"
-                    "safe-exceptions"
-                    "some"
-                    "stringsearch"
-                    "template-haskell"
-                    "terminal-size"
-                    "text-metrics"
-                    "time"
-                    "utf8-string"
-                  ];
-                  ghc-options = [
-                    "-W"
-                    "-Wno-name-shadowing"
-                    "-Wno-partial-type-signatures"
-                    "-Widentities"
-                    "-optP-Wno-nonportable-include-path"
-                    "-fdefer-typed-holes"
-                    "-fno-show-valid-hole-fits"
-                    "-fplugin=Effectful.Plugin"
-                    "-fwrite-ide-info"
-                    "-hiedir=.hie"
-                    "-O0"
-                    "-threaded"
-                    "-rtsopts"
-                    "-with-rtsopts=-N"
-                  ];
-                };
-                library = {
-                  enable = true;
-                  source-dirs = "src";
-
-                  component = {
-                    build-tools = [
-                      "alex"
+                      mdbook
+                      mdbook-d2
+                      d2
+                      nixd
                     ];
                   };
                 };
-                executables.elara = {
-                  source-dirs = "app";
-                  dependencies = [
-                    "autodocodec"
-                    "opt-env-conf"
-                  ];
-                  component.other-modules = [ "Paths_elara" ];
-                };
-                tests.elara-test = {
-                  main = "Spec.hs";
-                  default-extensions = [
-                    "QuasiQuotes"
-                  ];
-                  dependencies = [
-                    "HUnit"
-                    "QuickCheck"
-                    "hedgehog"
-                    "sydtest"
-                    "sydtest-hedgehog"
-                    "template-haskell"
-                    "neat-interpolation"
-                    "hspec-megaparsec"
-                  ];
-                  source-dirs = "test";
-                  component = {
-                    other-modules = [
-                      "Arbitrary.AST"
-                      "Arbitrary.Literals"
-                      "Arbitrary.Names"
-                      "Arbitrary.Type"
-                      "Boilerplate"
-                      "Common"
-                      "HedgehogSyd"
-                      "Golden"
-                      "Infer"
-                      "Infer.Unify"
-                      "Lex"
-                      "Lex.Common"
-                      "Lex.Indents"
-                      "LiftClosures"
-                      "Orphans"
-                      "Parse"
-                      "Parse.Common"
-                      "Parse.Expressions"
-                      "Parse.Patterns"
-                      "Region"
-                      "Shunt"
-                    ];
+
+                overrides =
+                  {
+                    source,
+                    hackage,
+                    enable,
+                    notest,
+                    unbreak,
+                    jailbreak,
+                    ...
+                  }:
+                  {
+                    h2jvm = notest (source.root h2jvm);
+                    unix = enable "os-string";
+                    directory = enable "os-string";
+                    diagnose = enable "megaparsec-compat" (source.root inputs.diagnose);
+                    incipit-base = jailbreak;
+                    incipit-core = jailbreak;
+                    ghc-tcplugins-extra = hackage "0.5" "sha256-mOzdicJevaXZdZS4/RA1hU3CWJXMFwMUfmEH3YxX4Q8=";
+
+                    optics-core = notest; # test fails on ghc 9.12
+                    optics = notest; # test fails on ghc 9.12
+                    generic-optics = notest; # test fails on ghc 9.12
+
+                    dependent-hashmap = source.root inputs.dependent-hashmap;
+
+                    effectful = jailbreak (hackage "2.6.1.0" "sha256-krNjGxqdbmFpt1g3anTd5ajGtYnyvGaG+AiDLfJN8No=");
+                    effectful-core = jailbreak;
+                    effectful-plugin = jailbreak;
+                    co-log-effectful = jailbreak (unbreak);
+                  };
+                packages = {
+                  elara = {
+                    buildInputs = pkgs: [ pkgs.alex ];
+                    src = ./.;
+                    description = "See README for more info";
+                    cabal = {
+                      author = "Alexander Wood";
+                      build-type = "Simple";
+                      copyright = "2022 Alexander Wood";
+                      license = "MIT";
+                      license-file = "LICENSE";
+                      version = "0.1.0";
+                      meta = {
+                        maintainer = "Alexander Wood <alexljwood24@hotmail.co.uk>";
+                        homepage = "https://github.com/ElaraLang/elara#readme";
+                        synopsis = "See README for more info";
+                      };
+                      language = "GHC2024";
+                      prelude = {
+                        enable = true;
+                        package = "relude";
+                        module = "Prelude";
+                      };
+                      default-extensions = [
+                        "OverloadedStrings"
+                        "OverloadedRecordDot"
+                        "TypeFamilies"
+                        "LambdaCase"
+                        "ImportQualifiedPost"
+                        "DeriveDataTypeable"
+                        "DataKinds"
+                        "DeriveFunctor"
+                        "TypeApplications"
+                        "PartialTypeSignatures"
+                      ];
+                      dependencies = [
+                        "aeson"
+                        "algebraic-graphs"
+                        "array"
+                        "binary"
+                        "bytestring"
+                        "co-log-core"
+                        "co-log-effectful"
+                        "containers"
+                        "dependent-sum-template"
+                        "diagnose"
+                        "directory"
+                        "dependent-hashmap"
+                        "effectful >= 2.6.1.0"
+                        "effectful-core"
+                        "effectful-plugin"
+                        "effectful-th"
+                        "filepath"
+                        "generic-optics"
+                        "h2jvm"
+                        "hashable"
+                        "kind-generics-th"
+                        "lens"
+                        "lifted-base"
+                        "matrix"
+                        "megaparsec"
+                        "mtl"
+                        "optics"
+                        "parser-combinators"
+                        "pretty-simple"
+                        "prettyprinter"
+                        "prettyprinter-ansi-terminal"
+                        "process"
+                        "relude"
+                        "safe-exceptions"
+                        "some"
+                        "stringsearch"
+                        "template-haskell"
+                        "terminal-size"
+                        "text-metrics"
+                        "time"
+                        "utf8-string"
+                      ];
+                      ghc-options = [
+                        "-W"
+                        "-Wno-name-shadowing"
+                        "-Wno-partial-type-signatures"
+                        "-Widentities"
+                        "-optP-Wno-nonportable-include-path"
+                        "-fdefer-typed-holes"
+                        "-fno-show-valid-hole-fits"
+                        "-fplugin=Effectful.Plugin"
+                        "-fwrite-ide-info"
+                        "-hiedir=.hie"
+                        "-O0"
+                        "-threaded"
+                        "-rtsopts"
+                        "-with-rtsopts=-N"
+                      ];
+                    };
+                    library = {
+                      enable = true;
+                      source-dirs = "src";
+
+                      component = {
+                        build-tools = [
+                          "alex"
+                        ];
+                      };
+                    };
+                    executables.elara = {
+                      source-dirs = "app";
+                      dependencies = [
+                        "autodocodec"
+                        "opt-env-conf"
+                      ];
+                      component.other-modules = [ "Paths_elara" ];
+                    };
+                    tests.elara-test = {
+                      main = "Spec.hs";
+                      default-extensions = [
+                        "QuasiQuotes"
+                      ];
+                      dependencies = [
+                        "HUnit"
+                        "QuickCheck"
+                        "hedgehog"
+                        "sydtest"
+                        "sydtest-hedgehog"
+                        "template-haskell"
+                        "neat-interpolation"
+                        "hspec-megaparsec"
+                      ];
+                      source-dirs = "test";
+                      component = {
+                        other-modules = [
+                          "Arbitrary.AST"
+                          "Arbitrary.Literals"
+                          "Arbitrary.Names"
+                          "Arbitrary.Type"
+                          "Boilerplate"
+                          "Common"
+                          "HedgehogSyd"
+                          "Golden"
+                          "Infer"
+                          "Infer.Unify"
+                          "Lex"
+                          "Lex.Common"
+                          "Lex.Indents"
+                          "LiftClosures"
+                          "Orphans"
+                          "Parse"
+                          "Parse.Common"
+                          "Parse.Expressions"
+                          "Parse.Patterns"
+                          "Region"
+                          "Shunt"
+                        ];
+                      };
+                    };
                   };
                 };
-              };
-            };
-          }
-        );
+              }
+            );
+          in
+          # Remove gen-overrides app: it evaluates cabal2nix derivations for all
+          # systems (including aarch64-darwin) which fails on x86_64-linux CI.
+          hixFlake
+          // {
+            apps = builtins.mapAttrs (_system: apps: builtins.removeAttrs apps [ "gen-overrides" ]) (
+              hixFlake.apps or { }
+            );
+          };
       }
     );
 }
